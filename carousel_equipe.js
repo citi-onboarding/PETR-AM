c9123d841355f064becee27281829da6d8df2a6a
dudasenna <39406516+dudasenna@users.noreply.github.com>
2018-11-28 01:47:24 -0200

    Add files via upload

--- carousel_equipe.js
+++ carousel_equipe.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
         slidesToScroll: 3,
         dots: true,
         arrows: true,
+        
+        // define configurações quando for mobile
         responsive: [
             {
               breakpoint: 750,
               settings: {
-                centerMode: true,
+                // centerMode: true,
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 infinite: true,
