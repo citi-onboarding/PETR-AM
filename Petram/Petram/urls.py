c9b15d67639f2cd7a66399825232848f8789844a
Lucas Aurelio Gomes Costa <lagc@ug3c07.windows.cin.ufpe.br>
2019-01-19 09:14:58 -0300

    ainda no bug do form

--- Petram/Petram/urls.py
+++ Petram/Petram/urls.py
@@ -4,8 +4,10 @@ from core.views import HomeView, ContactView
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'petram'
+
 urlpatterns = [
+    path('contato/', ContactView, name='contato'),
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('contato/', ContactView, name='contato'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
