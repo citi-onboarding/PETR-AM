1f657728c1372d047964f4f9286a8d420d5eebdc
lucas625 <lagc@cin.ufpe.br>
2019-02-09 20:00:55 -0300

    integração dropbox

--- Petram/settings/production.py
+++ Petram/settings/production.py
@@ -1,5 +1,9 @@
 from .base import *
-DEBUG = False
+DEBUG = True
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN = confi('ACCESS_TOKEN')
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
